0cdb6c9ba2b46ba62b65ce64f6edd3c6f0f89fa5
Mariia Paraskun <mariiaparaskun@gmail.com>
2023-11-24 10:42:12 +0100

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
+	try {
+		Bureaucrat fail("fail", 151);
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Operation on fail failed: " <<
+		e.what() << std::endl;
+	}
+
 	Bureaucrat one("*first*", 150);
 	Bureaucrat three("//three//", 1);
 	try {
@@ -31,6 +38,6 @@ int main()
 	std::cout << one; //ostream overload
 
 
-	//Bureaucrat two(one); to test copy
-	//three = one; to test copy
+	//Bureaucrat two(one); //to test copy
+	//three = one; //to test copy
 }
