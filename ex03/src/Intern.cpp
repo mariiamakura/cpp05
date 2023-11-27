8d3026b30938dee33156a3d67dceaddd09cc169a
mariiaparaskun <mariiaparaskun@gmail.com>
2023-11-27 12:04:11 +0100

    cpp05 finished

--- ex03/src/Intern.cpp
+++ ex03/src/Intern.cpp
@@ -11,7 +11,7 @@ Intern::Intern(const Intern &src) {
 
 Intern &Intern::operator=(const Intern &src) {
     if (this != &src)
-        this = src;
+        *this = src;
     std::cout << "%% Intern operator= called" <<std::endl;
     return (*this);
 }
@@ -20,15 +20,15 @@ Intern::~Intern() {
     std::cout << "%% Intern deleted" << std::endl;
 }
 
-static Form *makeScrubbery(const std::string &target) {
+static AForm *makeScrubbery(const std::string &target) {
     return (new ShrubberyCreationForm(target));
 }
 
-static Form *makeRobotomy(const std::string &target) {
+static AForm *makeRobotomy(const std::string &target) {
     return (new RobotomyRequestForm(target));
 }
 
-staticForm *makePresident(const std::string &target) {
+static AForm *makePresident(const std::string &target) {
     return (new PresidentialPardonForm(target));
 }
 
@@ -36,7 +36,7 @@ staticForm *makePresident(const std::string &target) {
 AForm *Intern::makeForm(const std::string &name, const std::string &target) {
     AForm *(*all_foo[3])(const std::string &target) = {&makeScrubbery, &makeRobotomy, &makePresident};
     const std::string form_name[3] = {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
-    for (int i = 0, i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         if (name == form_name[i])
         {
@@ -45,4 +45,5 @@ AForm *Intern::makeForm(const std::string &name, const std::string &target) {
         }
     }
     std::cout << RED << "Intern can't create " << name << RESET << std::endl;
+    return (NULL);
 }
--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -3,142 +3,29 @@
 #include "../include/ShrubberyCreationForm.hpp"
 #include "../include/RobotomyRequestForm.hpp"
 #include "../include/PresidentialPardonForm.hpp"
-
-
-
-void testPresident() {
-    PresidentialPardonForm sh_1("president");
-    std::cout << sh_1;
-
-    //can't exec, grade too low
-    try {
-        {
-            Bureaucrat b_one("-1-", 26);
-            sh_1.execute(b_one);
-        }
-    } catch (std::exception &e)
-    {
-        std::cout <<
-                  RED << e.what() << RESET << std::endl;
-    }
-
-    //can't exec since it's not signed
-    try {
-        {
-            Bureaucrat b_one("-1-", 5);
-            sh_1.execute(b_one);
-        }
-    } catch (std::exception &e)
-    {
-        std::cout << RED << e.what() << RESET << std::endl;
-    }
-
-    //test b function execForm
-    try {
-        Bureaucrat b_one("-1-", 6);
-        b_one.executeForm(sh_1);
-    } catch (std::exception &e)
-    {
-        std::cout << RED << e.what() << RESET << std::endl;
-    }
-    Bureaucrat b_one("-1-", 5);
-    sh_1.beSigned(b_one);
-    sh_1.execute(b_one);
-}
-
-void  testRobotomy() {
-    RobotomyRequestForm sh_1("robot");
-    std::cout << sh_1;
-
-    //can't exec, grade too low
-    try {
-        {
-            Bureaucrat b_one("-1-", 46);
-            sh_1.execute(b_one);
-        }
-    } catch (std::exception &e)
-    {
-        std::cout <<
-                  RED << e.what() << RESET << std::endl;
-    }
-
-    //can't exec since it's not signed
-    try {
-        {
-            Bureaucrat b_one("-1-", 20);
-            sh_1.execute(b_one);
-        }
-    } catch (std::exception &e)
-    {
-        std::cout << RED << e.what() << RESET << std::endl;
-    }
-
-    //test b function execForm
-    try {
-        Bureaucrat b_one("-1-", 20);
-        b_one.executeForm(sh_1);
-    } catch (std::exception &e)
-    {
-        std::cout << RED << e.what() << RESET << std::endl;
-    }
-
-
-
-    Bureaucrat b_one("-1-", 45);
-    sh_1.beSigned(b_one);
-    sh_1.execute(b_one);
-}
-
-void testShrubbery() {
-	ShrubberyCreationForm sh_1("Shrubbery");
-	std::cout << sh_1;
-
-	//can't exec, grade too low
-	try {
-		{
-			Bureaucrat b_one("-1-", 150);
-			sh_1.execute(b_one);
-		}
-	} catch (std::exception &e)
-	{
-		std::cout << 
-		RED << e.what() << RESET << std::endl;
-	}
-
-	//can't exec since it's not signed
-	try {
-		{
-			Bureaucrat b_one("-1-", 20);
-			sh_1.execute(b_one);
-		}
-	} catch (std::exception &e)
-	{
-		std::cout << RED << e.what() << RESET << std::endl;
-	}
-
-    //test b function execForm
-    try {
-            Bureaucrat b_one("-1-", 20);
-            b_one.executeForm(sh_1);
-    } catch (std::exception &e)
-    {
-        std::cout << RED << e.what() << RESET << std::endl;
-    }
-
-
-
-	Bureaucrat b_one("-1-", 130);
-	sh_1.beSigned(b_one);
-	sh_1.execute(b_one);
-}
+#include "../include/Intern.hpp"
 
 int main()
 {
-	//abstact class check
-	//AForm one("-111-", 1, 1);
+    Intern student;
+    Bureaucrat first("first", 20);
 
-	//testShrubbery();
-    //testRobotomy();
-    testPresident();
+    AForm *shrubbery = student.makeForm("ShrubberyCreationForm", "shrubbery");
+    std::cout << *shrubbery;
+    shrubbery->beSigned(first);
+    shrubbery->execute(first);
+    std::cout << *shrubbery << std::endl;
+
+    AForm *robotomy = student.makeForm("RobotomyRequestForm", "Robotomy");
+    std::cout << *robotomy;
+
+    AForm *presidential = student.makeForm("PresidentialPardonForm", "Presidential");
+    std::cout << *presidential;
+
+    student.makeForm("PresidetialPardonForm", "Presidetial");
+
+    delete (shrubbery);
+    delete (robotomy);
+    delete (presidential);
 
 }
